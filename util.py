840b4184b938ffcb3d41986401b8f3318199938e
A08rpita <arpitakhobragade2308@gmail.com>
2025-09-27 11:42:12 +0530

    Initial commit

---
+++ util.py
@@ -0,0 +1,66 @@
+import pickle
+import json
+import numpy as np
+import os
+
+# Globals
+__locations = None
+__data_columns = None
+__model = None
+
+# Build absolute paths relative to util.py
+BASE_DIR = os.path.dirname(__file__)
+COLUMNS_PATH = os.path.join(BASE_DIR, "artifacts", "columns.json")
+MODEL_PATH = os.path.join(BASE_DIR, "artifacts", "banglore_home_prices_model.pickle")
+
+def get_estimated_price(location, sqft, bhk, bath):
+    global __data_columns, __model
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0], 2)
+
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global __data_columns
+    global __locations
+    global __model
+
+    # Load columns.json
+    with open(COLUMNS_PATH, "r") as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
+
+    # Load model pickle
+    if __model is None:
+        with open(MODEL_PATH, "rb") as f:
+            __model = pickle.load(f)
+
+    print("loading saved artifacts...done")
+
+
+def get_location_names():
+    return __locations
+
+
+def get_data_columns():
+    return __data_columns
+
+
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2))  # other location
+    print(get_estimated_price('Ejipura', 1000, 2, 2))   # other location
